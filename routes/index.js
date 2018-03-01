7d9de61c428e007546be813839601af7462b800b
dec0mpiled <liimbix@gmail.com>
2018-03-01 15:56:40 +0000

    styles

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Save The Bees!' });
+});
+
+router.get('/facts', function(req, res, next) {
+  res.render('facts', { title: 'Facts About Bees' });
+});
+
+router.get('/donate', function(req, res, next) {
+  res.render('donate', { title: 'Donation' });
 });
 
 module.exports = router;
